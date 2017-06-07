4d464fc4d429ecd561efab1a65bfecc294126fcd
wangxiao <wangxiao@syswin.com>
2017-06-07 17:37:27 +0800

    提交脚本

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+#
+# usage:
+# > sh build.sh 1.0.0 200
+#
+
+versionNumber=$1 # 1.0.0
+buildNumber=$2 # 2000
+
+rm -rf build
+
+#basicLanes="AdHoc AppStore"
+basicLanes="AdHoc"
+for laneName in $basicLanes
+do
+fastlane $laneName version:$versionNumber build:$buildNumber
+done
+
+#channelIds="fir 91"
+#for channelId in $channelIds
+#do
+#fastlane Channel version:$versionNumber build:$buildNumber channel_id:$channelId
+#done
+
